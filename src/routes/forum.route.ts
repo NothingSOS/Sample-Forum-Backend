de5e4d6132be3a91399017693fa6acbb5403e61b
unknown <bestnothing@hotmail.com>
2022-11-04 17:26:45 +0900

    Update forum routes

--- src/routes/forum.route.ts
+++ src/routes/forum.route.ts
@@ -1,9 +1,31 @@
 import express from "express";
 
+import verifyToken from "../middlewares/verifyToken";
+
 const router = express.Router();
 
 router.get("/category", (req, res) => {
-  res.send("Forum Category!");
+  res.send("[GET] Get forum categories");
+});
+
+router.get("/post", (req, res) => {
+  res.send("[GET] Get forum posts");
+});
+
+router.post("/post", verifyToken, (req, res) => {
+  res.send("[POST] Create new forum post");
+});
+
+router.get("/post/:forumId", (req, res) => {
+  res.send(`[GET] Get forum post ID ${req.params.forumId} detail`);
+});
+
+router.get("/post/:forumId/reply", (req, res) => {
+  res.send(`[GET] Get forum replies for forum post ID ${req.params.forumId}`);
+});
+
+router.post("/post/:forumId/reply", verifyToken, (req, res) => {
+  res.send(`[POST] Create new forum reply for forum post ID ${req.params.forumId}`);
 });
 
 export default router;
