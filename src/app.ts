6e4976880965f2d408e9f40bed148f991c70bf64
unknown <bestnothing@hotmail.com>
2022-11-05 15:53:35 +0900

    Update security

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,16 @@ let myEnv = dotenv.config();
 dotenvExpand.expand(myEnv);
 
 import express from "express";
+import helmet from "helmet";
 import bodyParser from "body-parser";
 import { router } from "./routes";
 
 async function setup() {
   const app = express();
   const port = process.env.port ?? 3005;
+
+  app.use(helmet());
+  app.disable("x-powered-by");
 
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
